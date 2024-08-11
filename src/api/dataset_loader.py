c629b0281b48a0ba8b543d5cb049aedeb9a46716
Jonathen Sundy <jonathensundy@gmail.com>
2024-08-11 15:43:53 +0200

    Add dataset api interface

---
+++ src/api/dataset_loader.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class DatasetLoaderAPI(object):
+    def __init__(self, hf_dataset_names: List[str]):
+        # Fetch datasets from hf
+        pass
+
+    '''
+    preprocess the datasets defined in the class into prompt format
+    '''
+    def preprocess_dataset(self):
+        pass
