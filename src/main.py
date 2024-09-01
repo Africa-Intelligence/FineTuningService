f731a81eed9b917ad663378742be8f5e6df1b782
Chad Brouze <chadbrouze1@gmail.com>
2024-09-01 15:53:30 +0000

    Major code overhaul

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 import os
 
-from transformers import TrainingArguments
-from trl import SFTTrainer
-from unsloth import FastLanguageModel, is_bfloat16_supported
+import torch
+from huggingface_hub import login
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from peft import PeftModelForCausalLM, LoraConfig
+from trl import SFTTrainer, SFTConfig
 from dataset_processor import DatasetProcesser
 from llm_sample_cb import LLMSampleCB
 from dotenv import load_dotenv
 import yaml
 import wandb
 
+
 load_dotenv()
 assert "HF_API_KEY" in os.environ, "Please add your Hugging Face API key to the environment variables"
+login(token=os.environ["HF_API_KEY"])
 assert "WANDB_API_KEY" in os.environ, "Please add your Weights and Biases API key to the environment variables"
+wandb.login(token=os.environ["WANDB_API_KEY"])
 script_dir = os.path.dirname(os.path.abspath(__file__))
 config_path = os.path.join(script_dir, 'config.yaml')
 with open(config_path, 'r') as config_file:
@@ -23,18 +28,38 @@ train_dataset, eval_dataset, test_dataset = dataset_processor.get_processed_trai
     config['dataset_args']['train_split']
 )
 
-# Load model
-model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name=config['model_args']['model_name'],
-    dtype=None,
+# Define the quantization config
+quantization_config = BitsAndBytesConfig(
     load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.float16,  # or torch.bfloat16 if your GPU supports it
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4"
+)
+# Load model
+model = AutoModelForCausalLM.from_pretrained(
+    config['model_args']['model_name'],
+    quantization_config=quantization_config,
+    device_map="auto",
+    trust_remote_code=True,
     token=os.environ["HF_API_KEY"],
 )
+tokenizer = AutoTokenizer.from_pretrained(
+    config['model_args']['model_name'],
+    use_fast=True,
+    trust_remote_code=True,
+    token=os.environ["HF_API_KEY"],
+)
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+    model.config.pad_token_id = tokenizer.eos_token_id
 
 # Do model patching and add fast LoRA weights
-model = FastLanguageModel.get_peft_model(
-    model,
+lora_config = LoraConfig(
     r=config['model_args']['lora_rank'],
+    modules_to_save=None, # layers to unfreeze and train from the original pre-trained model
+    lora_alpha=config['model_args']['lora_alpha'],
+    lora_dropout=config['model_args']['lora_dropout'], 
+    bias=config['model_args']['bias'],
     target_modules=[
         "q_proj",
         "k_proj",
@@ -44,36 +69,39 @@ model = FastLanguageModel.get_peft_model(
         "up_proj",
         "down_proj",
     ],
-    modules_to_save=None, # layers to unfreeze and train from the original pre-trained model
-    lora_alpha=config['model_args']['lora_alpha'],
-    lora_dropout=config['model_args']['lora_dropout'], 
-    bias=config['model_args']['bias'],
-    use_gradient_checkpointing=True,
-    random_state=3407,
+    task_type="CAUSAL_LM",
+)
+model = PeftModelForCausalLM(
+    model,
+    peft_config=lora_config
 )
 
-os.environ['WANDB_PROJECT'] = 'llama3.1-8b-alpaca-fine-tuning'
+os.environ['WANDB_PROJECT'] = 'llama3.1-8b-Instruct-alpaca-fine-tuning'
 os.environ['WANDB_LOG_MODEL'] = 'checkpoint'
 # wandb stuff
-wandb.init(project='llama3.1-alpaca-fine-tuning', 
+wandb.init(project='llama3.1-8b-Instruct-alpaca-fine-tuning', 
             entity='africa-intelligence', 
             job_type="train",
             tags=['8b', 'hf_sft'],
             config=config)
 
 
-args = TrainingArguments(
-        per_device_train_batch_size = config['training_args']['per_device_train_batch_size'],
+args = SFTConfig(
+        dataset_text_field=config['dataset_args']['text_field'],
+        dataset_batch_size = config['training_args']['per_device_train_batch_size'],
         gradient_accumulation_steps = config['training_args']['gradient_accumulation_steps'],
         warmup_steps = config['training_args']['warmup_steps'],
         num_train_epochs = config['training_args']['epochs'],
         learning_rate = config['training_args']['learning_rate'],
-        fp16 = not is_bfloat16_supported(),
-        bf16 = is_bfloat16_supported(),
+        # fp16 = not is_bfloat16_supported(),
+        # bf16 = is_bfloat16_supported(),
         optim = config['training_args']['optimizer'],
         weight_decay = config['training_args']['weight_decay'],
         lr_scheduler_type = config['training_args']['lr_scheduler_type'],
+        # gradient_checkpointing=True,  # Enable gradient checkpointing
         seed = 3407,
+        packing=config['dataset_args']['packing'],  # pack samples together for efficient training
+        max_seq_length=config['dataset_args']['max_seq_length'],
         
         output_dir = "./output/",
         per_device_eval_batch_size=config['eval_args']['per_device_eval_batch_size'],
@@ -91,10 +119,7 @@ trainer = SFTTrainer(
     model,
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
-    packing=config['dataset_args']['packing'],  # pack samples together for efficient training
-    max_seq_length=config['dataset_args']['max_seq_length'],
     args=args,
-    dataset_text_field=config['dataset_args']['text_field']
 )
 
 wandb_callback = LLMSampleCB(trainer, test_dataset, num_samples=1, max_new_tokens=256)
